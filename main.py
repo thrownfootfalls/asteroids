294710152248f9547fa38ab427093d0d78c12c69
thrownfootfalls <alastairrgmurray@gmail.com>
2025-01-17 13:29:13 +0000

    Game window working

---
+++ main.py
@@ -0,0 +1,23 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import * # I've been instructed not to worry about the wildcard for this project
+
+pygame.init()
+
+def main():
+    print("Starting asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen, (0,0,0))
+    pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
