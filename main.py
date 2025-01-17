c5bfba180e3e58aabae1ebde2401a6e742805ed3
thrownfootfalls <alastairrgmurray@gmail.com>
2025-01-17 18:19:47 +0000

    added CircleShape class; ready for check 2.4

--- main.py
+++ main.py
@@ -4,12 +4,13 @@
 import pygame
 from constants import * # I've been instructed not to worry about the wildcard for this project
 
-pygame.init()
-
 def main():
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -17,7 +18,9 @@ def main():
                 return
 
         pygame.Surface.fill(screen, (0,0,0))
-    pygame.display.flip()
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
